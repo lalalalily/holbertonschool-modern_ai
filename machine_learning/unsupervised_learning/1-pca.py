ce6d6495fe52c33f02e56d300d058c446bb501f4
lalalalily <ranaarasulzade@gmail.com>
2026-05-28 19:35:06 +0000

    new

--- machine_learning/unsupervised_learning/1-pca.py
+++ machine_learning/unsupervised_learning/1-pca.py
@@ -9,6 +9,8 @@ def Apply_PCA(X, n_components, random_state):
     """
     Performs PCA on tabular data
     """
-    pca = decomposition.PCA(n_components=n_components, random_state=random_state)
+    pca = decomposition.PCA(
+        n_components=n_components, random_state=random_state
+    )
     X_pca = pca.fit_transform(X)
     return X_pca, pca
